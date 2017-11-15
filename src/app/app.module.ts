9d0714bc3fc95894ee13348310f81292100e82ad
Philip Ekblom <philip.ekblom@gmail.com>
2017-11-15 17:29:03 +0100

    Cleanup and loading icon added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,9 @@ import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import {RouterModule} from '@angular/router';
 
-import {PostsService} from './posts/posts.service';
 import {CvService} from './cv/cv.service';
 
 import {AppComponent} from './app.component';
-import {PostsComponent} from './posts/posts.component';
 import {ROUTES} from './app.routes';
 import {CvComponent} from './cv/cv.component';
 import { LoopPipe } from './tools/loop.pipe';
@@ -16,7 +14,6 @@ import { LoopPipe } from './tools/loop.pipe';
 @NgModule({
     declarations: [
         AppComponent,
-        PostsComponent,
         CvComponent,
         LoopPipe
     ],
@@ -27,7 +24,6 @@ import { LoopPipe } from './tools/loop.pipe';
         RouterModule.forRoot(ROUTES)
     ],
     providers: [
-        PostsService,
         CvService
     ],
     bootstrap: [AppComponent]
