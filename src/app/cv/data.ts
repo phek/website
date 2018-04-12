aa7c649ab0d6270b36accf0266c69fbab7247ca9
Philip Ekblom <philip.ekblom@gmail.com>
2018-04-12 11:02:28 +0200

    CV Updated

--- src/app/cv/data.ts
+++ src/app/cv/data.ts
@@ -1,179 +1,188 @@
 export const EDUCATION = [
     {
-        "school": "KTH Kista",
-        "area": "Computer Science Engineering",
-        "individual": [
-            "Ethical Hacking",
-            "Human Computer Interaction (HCI)",
-            "Computer Graphics"
+        'school': 'KTH Kista',
+        'area': 'Computer Science Engineering',
+        'individual': [
+            'Ethical Hacking',
+            'Human Computer Interaction (HCI)',
+            'Computer Graphics'
         ]
     },
     {
-        "school": "Nacka Gymnasium",
-        "area": "Computer Technology",
-        "individual": [
-            "Psychology",
-            "Economy",
+        'school': 'Nacka Gymnasium',
+        'area': 'Computer Technology',
+        'individual': [
+            'Psychology',
+            'Economy',
+            'Digital Design'
         ],
-        "extras": [
-            "Cisco Network Certificate"
+        'extras': [
+            'Cisco Network Certificate'
         ]
     }
 ];
 
 export const EXPERIENCES = [
     {
-        "category": "Website languages",
-        "items": [
+        'category': 'Website languages',
+        'items': [
             {
-                "title": "HTML",
-                "xp": 4
+                'title': 'HTML',
+                'xp': 4
             },
             {
-                "title": "CSS",
-                "xp": 4
+                'title': 'CSS',
+                'xp': 4
             },
             {
-                "title": "Javascript",
-                "xp": 4
+                'title': 'Javascript',
+                'xp': 4
             },
             {
-                "title": "PHP",
-                "xp": 3
+                'title': 'PHP',
+                'xp': 3
             }
         ]
     },
     {
-        "category": "Javascript",
-        "items": [
+        'category': 'Javascript',
+        'items': [
             {
-                "title": "Jquery",
-                "xp": 4
+                'title': 'Jquery',
+                'xp': 4
             },
             {
-                "title": "AngularJS",
-                "xp": 3
+                'title': 'AngularJS',
+                'xp': 3
             },
             {
-                "title": "ReactJS",
-                "xp": 1
+                'title': 'ReactJS',
+                'xp': 2
             },
             {
-                "title": "NodeJS",
-                "xp": 2
+                'title': 'React-Native',
+                'xp': 2
             },
             {
-                "title": "MongoDB",
-                "xp": 2
+                'title': 'NodeJS',
+                'xp': 2
             },
             {
-                "title": "Socket.io",
-                "xp": 3
+                'title': 'MongoDB',
+                'xp': 2
             },
             {
-                "title": "JWT",
-                "xp": 2
+                'title': 'Socket.io',
+                'xp': 3
+            },
+            {
+                'title': 'JWT',
+                'xp': 2
             }
         ]
     },
     {
-        "category": "Programming languages",
-        "items": [
+        'category': 'Programming languages',
+        'items': [
             {
-                "title": "Java",
-                "xp": 3
+                'title': 'Java',
+                'xp': 3
             },
             {
-                "title": "C/C++",
-                "xp": 1
+                'title': 'C/C++',
+                'xp': 1
             },
             {
-                "title": "Erlang",
-                "xp": 1
+                'title': 'Python',
+                'xp': 1
             },
             {
-                "title": "Lua",
-                "xp": 2
+                'title': 'Erlang',
+                'xp': 1
             },
             {
-                "title": "Assembly",
-                "xp": 1
+                'title': 'Lua',
+                'xp': 1
             },
             {
-                "title": "Scala",
-                "xp": 1
+                'title': 'Assembly',
+                'xp': 1
+            },
+            {
+                'title': 'Scala',
+                'xp': 1
             }
         ]
     },
     {
-        "category": "Media programs",
-        "items": [
+        'category': 'Media programs',
+        'items': [
             {
-                "title": "Microsoft Office",
-                "xp": 4
+                'title': 'Microsoft Office',
+                'xp': 4
             },
             {
-                "title": "Adobe Photoshop",
-                "xp": 4
+                'title': 'Adobe Photoshop',
+                'xp': 4
             },
             {
-                "title": "Adobe Illustrator",
-                "xp": 3
+                'title': 'Adobe Illustrator',
+                'xp': 3
             },
             {
-                "title": "Adobe InDesign",
-                "xp": 2
+                'title': 'Adobe InDesign',
+                'xp': 2
             },
             {
-                "title": "Adobe After Effects",
-                "xp": 2
+                'title': 'Adobe After Effects',
+                'xp': 2
             },
             {
-                "title": "Sony Vegas Pro",
-                "xp": 2
+                'title': 'Sony Vegas Pro',
+                'xp': 2
             }
         ]
     },
     {
-        "category": "Game engines",
-        "items": [
+        'category': 'Game engines',
+        'items': [
             {
-                "title": "Unity",
-                "xp": 3
+                'title': 'Unity',
+                'xp': 2
             },
             {
-                "title": "Unreal Engine",
-                "xp": 2
+                'title': 'Unreal Engine',
+                'xp': 2
             }
         ]
     },
     {
-        "category": "Modelling programs",
-        "items": [
+        'category': 'Modelling programs',
+        'items': [
             {
-                "title": "Blender",
-                "xp": 3
+                'title': 'Blender',
+                'xp': 2
             },
             {
-                "title": "Autodesk Maya",
-                "xp": 2
+                'title': 'Autodesk Maya',
+                'xp': 2
             },
             {
-                "title": "Cinema 4D",
-                "xp": 2
+                'title': 'Cinema 4D',
+                'xp': 2
             }
         ]
     },
     {
-        "category": "Music production",
-        "items": [
+        'category': 'Music production',
+        'items': [
             {
-                "title": "FL Studio",
-                "xp": 4
+                'title': 'FL Studio',
+                'xp': 4
             },
             {
-                "title": "Traktor Pro",
-                "xp": 2
+                'title': 'Traktor Pro',
+                'xp': 2
             }
         ]
     }
